a9ca5f39320261775a1b346cb43bc285f9c5e6a4
Rudi Chen <rudichen@gmail.com>
2016-11-18 22:34:58 -0800

    Parse messages

--- src/analysis/analyzer.ts
+++ src/analysis/analyzer.ts
@@ -1,4 +1,7 @@
 import * as parse5 from 'parse5';
+import * as _ from 'underscore';
+import * as d3 from 'd3-time-format';
+
 type TextNode = parse5.AST.Default.TextNode
 type ASTElement = parse5.AST.Default.Element;
 
@@ -51,7 +54,8 @@ class DomNode {
         let text_fragments =
             this.ast.childNodes
                 .filter(child => child.nodeName === "#text")
-                .map(child => (child as TextNode).value.trim());
+                .map(child => (child as TextNode).value.trim())
+                .filter(fragment => fragment.length > 0);
         return text_fragments.join("\n");
     }
 
@@ -68,6 +72,59 @@ class DomNode {
     }
 }
 
+class Message {
+    author: string;
+    time: Date;
+    text: string;
+
+    constructor(author: string, time: Date, text: string) {
+        this.author = author;
+        this.time = time;
+        this.text = text;
+    }
+}
+
+class MessageThread {
+    parties: string[];
+    messages: Message[]
+
+    constructor(parties: string[], messages: Message[]) {
+        this.parties = parties;
+        this.messages = messages;
+    }
+}
+
+let _parseTimePlain = d3.timeParse("%A, %B %d, %Y at %I:%M%p");
+let _parseTimePST = d3.timeParse("%A, %B %d, %Y at %I:%M%p PST");
+let _parseTimePDT = d3.timeParse("%A, %B %d, %Y at %I:%M%p PDT");
+function parseTime(raw: string): Date {
+    // TODO: Think about the PST/PDT parsing issue.
+    return _parseTimePlain(raw);
+}
+
+function readMessage(info: DomNode, text: string): Message {
+    let author = info.getFirstByClass("user").text();
+    let time = parseTime(info.getFirstByClass("meta").text());
+    return new Message(author, time, text);
+}
+
+function readMessageThread(node: DomNode): MessageThread {
+    // TODO: Edge case: What if people have a comma in their names?
+    let people = node.text().split(",");
+    let messageInfos =
+        node.getAllByClass("message")
+            .map(node => node.getFirstByClass("message_header"));
+    let messageTexts = node.getAllByTag("p").map(node => node.text());
+
+    if (messageInfos.length != messageTexts.length) {
+        console.error("Number of message infos not the same as messages");
+    }
+
+    let messages = _.zip(messageInfos, messageTexts)
+                    .map(pair => readMessage(pair[0], pair[1]));
+    return new MessageThread(people, messages);
+}
+
 function parseRaw(raw: string) {
     let start = new Date().getTime();
     let document = parse5.parse(raw) as parse5.AST.Default.Document;
@@ -81,6 +138,13 @@ onmessage = function (message) {
     let dom = parseRaw(String(message.data));
     let contents = dom.body().getFirstByClass("contents");
     let name = contents.h1().text();
-    let threads = contents.div().getAllByClass("thread");
-    (postMessage as any)({ key :"name", value: name });
+
+    // Not sure what critera determines how <div class="thread"> are grouped
+    // into unlabelled <div>s, but it happens.
+    let threads_groups = contents.getAllByTag("div")
+                                 .map(group => group.getAllByClass("thread"));
+    let threads = [].concat.apply([], threads_groups);
+    threads.map(readMessageThread);
+
+    (postMessage as any)({ key: "name", value: name });
 }
